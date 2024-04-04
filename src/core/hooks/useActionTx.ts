753ff28fdf2909c928133e8a4e63c2f077e4e4e3
lukepark327 <lukepark327@gmail.com>
2024-04-04 15:25:52 +0900

    feat: error treating

--- src/core/hooks/useActionTx.ts
+++ src/core/hooks/useActionTx.ts
@@ -16,53 +16,61 @@ export const useActionTx = (projectList: ProjectType[]) => {
     async (walletAddress: string, projectId: number, input: string) => {
       if (!cwClient) return null;
       const copyClient = _.cloneDeep(cwClient);
-      const result = await copyClient.execute(
-        userAddress,
-        ContractInfo.contractAddr,
-        {
-          ResultRequestMsg: {
-            user: walletAddress,
-            id: projectId,
-            input: input,
+
+      console.log(walletAddress, projectId, input);
+      try {
+        const result = await copyClient.execute(
+          userAddress,
+          ContractInfo.contractAddr,
+          {
+            ResultRequestMsg: {
+              user: walletAddress,
+              id: projectId,
+              input: input,
+            },
           },
-        },
-        "auto"
-      );
+          "auto"
+        );
 
-      if (result) {
-        // const newActionResults = [...actionResults];
-        // newActionResults[projectId] =
-        //   "Project Gateway is a groundbreaking solution designed to bridge the gap between Web 2.0 and Web 3.0, built on the Archway platform.";
-        // setActionResults(newActionResults);
+        if (result) {
+          // const newActionResults = [...actionResults];
+          // newActionResults[projectId] =
+          //   "Project Gateway is a groundbreaking solution designed to bridge the gap between Web 2.0 and Web 3.0, built on the Archway platform.";
+          // setActionResults(newActionResults);
 
-        let intervalId: NodeJS.Timeout;
-        const fetchData = async () => {
-          try {
-            const response = await fetch(`http://localhost:3327/api/${projectId}`);
-            const data = await response.json();
-            const parsed = JSON.parse(data.data);
+          let intervalId: NodeJS.Timeout;
+          const fetchData = async () => {
+            try {
+              const response = await fetch(`http://localhost:3327/api/${projectId}`);
+              const data = await response.json();
+              const parsed = JSON.parse(data.data);
 
-            // Check if the fetch was successful
-            if (parsed && parsed.result) {
-              const newActionResults = [...actionResults];
-              newActionResults[projectId] = parsed.result;
-              setActionResults(newActionResults);
+              // Check if the fetch was successful
+              if (parsed && parsed.result) {
+                const newActionResults = [...actionResults];
+                newActionResults[projectId] = parsed.result;
+                setActionResults(newActionResults);
 
-              clearInterval(intervalId);
-            } else {
-              console.error("Not yet", projectId);
+                clearInterval(intervalId);
+              } else {
+                console.error("Not yet", projectId);
+              }
+            } catch (error) {
+              console.error("Fetch failed", projectId, error);
             }
-          } catch (error) {
-            console.error("Fetch failed", projectId, error);
-          }
-        };
-        fetchData();
-        intervalId = setInterval(fetchData, 10000);
-      } else {
-        console.error("Error Tx");
-      }
+          };
+          fetchData();
+          intervalId = setInterval(fetchData, 10000);
+        } else {
+          console.error("Error Tx");
+        }
 
-      return result;
+        return result;
+      } catch (error) {
+        console.error(error);
+
+        return null;
+      }
     },
     [cwClient, userAddress, actionResults]
   );
